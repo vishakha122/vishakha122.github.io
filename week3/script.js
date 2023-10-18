e93049bbfc6a2e6b90614fb3a202ffad7b98e7f0
Vishakha Heralal <vh2008@hw.ac.uk>
2023-10-19 01:17:14 +0400

    Update script.js

--- week3/script.js
+++ week3/script.js
@@ -35,70 +35,70 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const cPassword = document.getElementById("c-password");
-//Show Error Message
-function showError(input, message) {
-  const formControl = input.parentElement;
-  formControl.className = "input error";
-  const small = formControl.querySelector("small");
-  small.innerText = message;
-}
-//Show Success message
-function showSuccess(input) {
-  const formControl = input.parentElement;
-  formControl.classList.add("success");
-}
-//Check Required fields
-function checkRequired(inputArr) {
-  inputArr.forEach(function (input) {
-    if (input.value.trim() === "") {
-      showError(input, `${getFieldName(input)} is required`);
-    } else {
-      showSuccess(input);
-    }
-  });
-}
-// Get Field Name
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-// Check Input Lenght
-function checkLenghth(input, min, max) {
-  if (input.value.length < min) {
-    showError(
-      input,
-      `${getFieldName(input)} must be at least ${min} characters `
-    );
-  } else if (input.value.length > max) {
-    showError(
-      input,
-      `${getFieldName(input)} must be less than ${max} characters `
-    );
-  } else {
-    showSuccess(input);
-  }
-}
-// Check E-mail Is Valid
-function checkEmail(input) {
-  const re =
-    /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
-  if (re.test(input.value.trim())) {
-    showSuccess(input);
-  } else {
-    showError(input, "E-mail is not Valid");
-  }
-}
-// Check Password Match
-function checkPasswordMatch(input1, input2) {
-  if (input1.value !== input2.value) {
-    showError(input2, "Passwords do not match");
-  }
-}
-form.addEventListener("submit", (e) => {
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", e => {
   e.preventDefault();
-  checkRequired([username, email, password, cPassword]);
-  checkLenghth(username, 3, 15);
-  checkLenghth(password, 8, 25);
-  checkEmail(email);
-  checkPasswordMatch(password, cPassword);
+  checkInputs();
 });
+
+function checkInputs() {
+  //Get the value the form field.
+  const usernameValue = username.value.trim(); //trim to delete blanc space.
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === "") {
+    setErrorInput(username, "UserName cannot be blank.");
+  } else {
+    setSuccessInput(username);
+  }
+
+  //###################################
+  if (emailValue === "") {
+    setErrorInput(email, "Email cannot be blank.");
+  } else {
+    validateEmail(emailValue) && setSuccessInput(email);
+  }
+
+  //###################################
+  if (passwordValue === "") {
+    setErrorInput(password, "Password connot be blank.");
+  } else {
+    setSuccessInput(password) && validatePassword(passwordValue);
+  }
+
+  //###################################
+  if (password2Value === "") {
+    setErrorInput(password2, "Password connot be blank.");
+  } else if (password2Value !== passwordValue) {
+    setErrorInput(password2, "Password dose not match.");
+  } else {
+    setSuccessInput(password2);
+  }
+}
+
+function setErrorInput(input, errorMessage) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = errorMessage;
+  formControl.className = "form__control error";
+}
+
+function setSuccessInput(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form__control success";
+}
+
+function validateEmail(email) {
+  let regular_expressions = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regular_expressions.test(String(email).toLocaleLowerCase());
+}
+
+function validatePassword(password) {
+  let regular_expressions = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+  return regular_expressions.match(regular_expressions);
+}
+
