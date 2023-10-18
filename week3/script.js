df1864788c6a76f0746441da89dc8bdd4f48cb97
Vishakha Heralal <vh2008@hw.ac.uk>
2023-10-19 00:59:33 +0400

    Update script.js

--- week3/script.js
+++ week3/script.js
@@ -35,71 +35,70 @@ const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
-
+const cPassword = document.getElementById("c-password");
+//Show Error Message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "input error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+//Show Success message
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.classList.add("success");
+}
+//Check Required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+// Get Field Name
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+// Check Input Lenght
+function checkLenghth(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters `
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters `
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+// Check E-mail Is Valid
+function checkEmail(input) {
+  const re =
+    /^(([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "E-mail is not Valid");
+  }
+}
+// Check Password Match
+function checkPasswordMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
-  validateInputs();
+  checkRequired([username, email, password, cPassword]);
+  checkLenghth(username, 3, 15);
+  checkLenghth(password, 8, 25);
+  checkEmail(email);
+  checkPasswordMatch(password, cPassword);
 });
-
-const setError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
-
-  errorDisplay.innerText = message;
-  inputControl.classList.add("error");
-  inputControl.classList.remove("success");
-};
-
-const setSuccess = (element) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
-
-  errorDisplay.innerText = "";
-  inputControl.classList.add("success");
-  inputControl.classList.remove("error");
-};
-
-const isValidEmail = (email) => {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-};
-
-const validateInputs = () => {
-  const usernameValue = username.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
-  const password2Value = password2.value.trim();
-
-  if (usernameValue === "") {
-    setError(username, "Username is required");
-  } else {
-    setSuccess(username);
-  }
-
-  if (emailValue === "") {
-    setError(email, "Email is required");
-  } else if (!isValidEmail(emailValue)) {
-    setError(email, "Provide a valid email address");
-  } else {
-    setSuccess(email);
-  }
-
-  if (passwordValue === "") {
-    setError(password, "Password is required");
-  } else if (passwordValue.length < 8) {
-    setError(password, "Password must be at least 8 character.");
-  } else {
-    setSuccess(password);
-  }
-
-  if (password2Value === "") {
-    setError(password2, "Please confirm your password");
-  } else if (password2Value !== passwordValue) {
-    setError(password2, "Passwords doesn't match");
-  } else {
-    setSuccess(password2);
-  }
-};
