ce6cda5b5f41f2ffd450cbd9f4cd2d0223706f48
Vishakha Heralal <vh2008@hw.ac.uk>
2023-11-08 19:49:30 +0400

    updated code

---
+++ week4/app.js
@@ -0,0 +1,40 @@
+const cityInput = document.getElementById("cityInput");
+const btn = document.getElementById("btn");
+const weatherInfo = document.getElementById("weather-info");
+
+btn.addEventListener("click", function () {
+  // Step 4: Check for an empty city input
+  const city = cityInput.value.trim(); // Trim to remove leading/trailing spaces
+  if (city === "") {
+    alert("Please enter a city name.");
+    return; // Exit the function
+  }
+
+  // Step 5: Make an HTTP request to OpenWeatherMap API
+  const apiKey = "d8a8fed8af30894a36e77b8e3b580867"; // Replace with your actual API key
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // Step 7: Update the weather info div with weather details
+      const weatherDescription = data.weather[0].description;
+      const temperature = data.main.temp;
+      const windSpeed = data.wind.speed;
+
+      const htmlString = `<p>Weather: ${weatherDescription}</p>
+                          <p>Temperature: ${temperature}°C</p>
+                          <p>Wind Speed: ${windSpeed} m/s</p>`;
+
+      weatherInfo.innerHTML = htmlString;
+    })
+    .catch((error) => {
+      // Step 6: Handle errors
+      console.error("An error occurred:", error);
+    });
+});
