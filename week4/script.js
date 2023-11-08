aa1b97dc2e2ce40bf5d8626d71fff00c5f95049a
Vishakha Heralal <vh2008@hw.ac.uk>
2023-11-08 19:33:16 +0400

    Update script.js

--- week4/script.js
+++ week4/script.js
@@ -32,3 +32,23 @@ function renderHTML(data) {
   var htmlString = "this is a test";
   cityContainer.insertAdjacentHTML("beforeend", htmlString);
 }
+
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open("GET", "https://vishakha122.github.io/week4/cities1.json");
+  ourRequest.onload = function () {
+    var ourData = JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+  };
+  ourRequest.send();
+});
+function renderHTML(data) {
+  var htmlString = "";
+  for (i = 0; i < data.length; i++) {
+    htmlString +=
+      "<p>" + data[i].name + " is a city in " + data[i].country + ".</p>";
+  }
+  cityContainer.insertAdjacentHTML("beforeend", htmlString);
+}
