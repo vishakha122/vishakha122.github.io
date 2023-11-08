ca8e5c86a8a40ebca540fd5695b546ffaa380308
Vishakha Heralal <vh2008@hw.ac.uk>
2023-11-08 19:27:41 +0400

    updated code

--- week4/script.js
+++ week4/script.js
@@ -1,8 +1,5 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open(
-  "GET",
-  "https://vishakha122.github.io/week4/cities1.json"
-);
+ourRequest.open("GET", "https://vishakha122.github.io/week4/cities1.json");
 ourRequest.onload = function () {
   var ourData = JSON.parse(ourRequest.responseText);
   console.log(ourData[0]);
