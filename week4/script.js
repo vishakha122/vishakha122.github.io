2083025f4916311b564965f82c9ded7c0ba4fc53
Vishakha Heralal <vh2008@hw.ac.uk>
2023-11-08 19:00:46 +0400

    added code

---
+++ week4/script.js
@@ -0,0 +1,6 @@
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://your-github-username.github.io/your-repository￾name/cities1.json');
+ourRequest.onload = function() {
+console.log(ourRequest.responseText);
+};
+ourRequest.send();
